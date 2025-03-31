e93a08a4d055fddcfbd04e259b94fa484fcd5578
makiroll.mark <delacruzmacky39@gmail.com>
2025-03-31 09:53:43 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+//Cadag, Jaycee
+//Dela Cruz, Mark 
+//Espinas, A Z Rain
+//Lovina, John Melrick
+//Morcozo, Janna
+
+#include <stdio.h>
+#include <time.h>
+
+//Function Prototypes
+void selectionSort (int[] input);
+void bubbleSort (int[] input);
+void insertionSort (int[] input);
+void mergeSort (int[] input);
+void quickSort (int[] input);
+void heapSort (int[] input);
+
+int main(){
+
+}
